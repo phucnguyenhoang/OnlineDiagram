c1e1b88d622428204dcfedd66a20e17bfc0a4062
vokhoanam <vokhoanam@gmail.com>
2013-10-12 19:06:04 +0700

    update source code

--- js/core_object.js
+++ js/core_object.js
@@ -1,32 +1,47 @@
-var Config = {
-    tableTitle: 'Table Name',
-    tableTitleColor: '#47423F',
-    tableTitleBgColor: '#527578'
-}
-
+var Config = new Default();
 function Table(r, x, y) {
-    this.paper = r;   
-    this.title = 'Table Name';    
+    this.paper = r;  
     this.x = x;
     this.y = y;
+    this.w = Config.tableWidth;
+    this.h = Config.tableHeight;
+    this.titleText = Config.tableTitle;    
+    this.titleSize = Config.tableTitleSize;
+    this.titleFill = Config.tableTitleBgColor;
+    this.titleColor = Config.tableTitleColor;
     this.rect;
-}
+    this.title;
+};
 
 Table.prototype.draw = function() {
-    var paper = this.paper;
-    var rect = paper.rect(this.x, this.y, 150, 200);
-    var titleBorder = paper.rect(this.x, this.y, 150, 30);
-    titleBorder.attr({fill: ''});
-    this.rect = rect;
-}
+    var paper = this.paper,
+        x = this.x,
+        y = this.y,
+        w = this.w,
+        h = this.h,
+        fill = this.titleFill;
+    var rect = paper.rect(x, y, w, h);
+    var titleBorder = paper.rect(x, y, w, 30).attr({fill: fill});
+    var s = paper.set();
+    s.push(rect, titleBorder);
+    this.rect = s;
+};
 
-Table.prototype.title = function() {
-    var paper = this.paper;
-    var box;
-}
+Table.prototype.setTitle = function() {
+    var paper = this.paper,
+        x = this.x + parseInt(this.w/2),
+        y = this.y + 15,
+        title = this.titleText,
+        size = this.titleSize,
+        color = this.titleColor;
+    
+    var txt = paper.text(x, y, title).attr({stroke: color, 'font-size': size});
+    this.title = txt;
+};
 
 Table.prototype.init = function() {
     var self = this;
     self.draw();
-}
+    self.setTitle();
+};
 
---
+++ js/default.js
@@ -0,0 +1,8 @@
+function Default() {
+    this.tableTitle = 'Table Name',
+    this.tableTitleColor = '#47423F',
+    this.tableTitleBgColor = '#527578',
+    this.tableTitleSize = '15px',
+    this.tableWidth = 150,
+    this.tableHeight = 200
+};
